0ce9ac0b584d3ab8b102e746ee6dd157ff55223a
Jon Smusev <you@example.com>
2020-07-20 01:03:17 +0300

    Updates

---
+++ precache-manifest.e9295ec7cd8b5eb36e370dceed99d3da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8441db91d633fbf8fcd7001320072a25",
+    "url": "/react-booking/index.html"
+  },
+  {
+    "revision": "c50ef6923eaf9ba8103e",
+    "url": "/react-booking/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "b122f83039c5ca62a938",
+    "url": "/react-booking/static/css/main.66d2654c.chunk.css"
+  },
+  {
+    "revision": "c50ef6923eaf9ba8103e",
+    "url": "/react-booking/static/js/2.107b0c46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-booking/static/js/2.107b0c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b122f83039c5ca62a938",
+    "url": "/react-booking/static/js/main.26194844.chunk.js"
+  },
+  {
+    "revision": "e5db6a9f93b8914aa405",
+    "url": "/react-booking/static/js/runtime-main.82cb330d.js"
+  },
+  {
+    "revision": "29dd6ddf28f58d03c540e0e1c2db023b",
+    "url": "/react-booking/static/media/Privat24_QR.29dd6ddf.jpg"
+  },
+  {
+    "revision": "5212d7e4050b8adfc0d4e62f7e66c34d",
+    "url": "/react-booking/static/media/carScheme.5212d7e4.png"
+  }
+]);
--- src/components/data/index.js
+++
@@ -1,7 +0,0 @@
-export const cities = [
-  {label: 'Київ', value: 'Київ'},
-  {label: 'Харків', value: 'Харків'},
-  {label: 'Львів', value: 'Львів'},
-  {label: 'Дніпро', value: 'Дніпро'},
-  {label: 'Одеса', value: 'Одеса'},
-];
